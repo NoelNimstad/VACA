6022c9e64228eeaab3045dd2847845b3d91eb33f
noelnimstad <noel@nimstad.com>
2024-10-19 15:04:26 +0200

    fixed MacOS openFileDialog method

--- source/VACA/osx/VACA_fileDialog.m
+++ source/VACA/osx/VACA_fileDialog.m
@@ -9,10 +9,12 @@ const char* VACA_OSX_openFileDialog(void)
         [panel setAllowsMultipleSelection:NO];
         [panel setCanChooseDirectories:NO];
 
-        if([panel runModal] == NSModalResponseOK)
+        if([panel runModal] == NSModalResponseOK) 
         {
             NSURL* chosenFile = [[panel URLs] objectAtIndex:0];
-            return [chosenFile.path UTF8String];
+            static char file_path[512];
+            strncpy(file_path, [chosenFile.path UTF8String], sizeof(file_path));
+            return file_path;
         }
     }
     return NULL;
